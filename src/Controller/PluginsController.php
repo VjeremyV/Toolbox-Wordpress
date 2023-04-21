8cde0c2ffdbb3d4eb08bbce843359be32a8246e2
Jeremy WA <jeremy-vaugoyeau@web-alliance.fr>
2023-04-21 16:06:04 +0200

    récupératon readme

---
+++ src/Controller/PluginsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PluginsController extends AbstractController
+{
+    #[Route('/plugins', name: 'app_plugins')]
+    public function index(): Response
+    {
+        return $this->render('plugins/plugin.html.twig', [
+            'controller_name' => 'PluginsController',
+        ]);
+    }
+}
