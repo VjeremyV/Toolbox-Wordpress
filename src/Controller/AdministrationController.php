2d99fb16f49543db09542f7e89b5bc5a2f8d4f6f
Jeremy WA <jeremy-vaugoyeau@web-alliance.fr>
2023-05-02 16:49:13 +0200

    ajout utilisateur ok

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -2,15 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\UsersFormType;
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdministrationController extends AbstractController
 {
     #[Route('/administration', name: 'app_administration')]
-    public function index(): Response
+    public function index(UsersRepository $usersRepository, Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('administration/administration.html.twig');
+        $role = "";
+        $users = $usersRepository->findAll();
+        $user = new Users();
+        $form = $this->createForm(UsersFormType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('info', 'L\'utilisateur a bien été ajouté');
+            $pwd = $passwordHasher->hashPassword($user, $user->getPassword()); // pour hasher le pwd
+            $user->setPassword($pwd);
+            $usersRepository->save($user, true);
+        }
+        return $this->render(
+            'administration/administration.html.twig',
+            [
+                'users' => $users,
+                'form' => $form->createView(),
+                'role' => $role
+            ]
+        );
     }
 }
